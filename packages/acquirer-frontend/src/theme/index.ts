3cade8a601464061af2cdf407d7418620f8acaf9
nainglinnkhant <nainglinnkhnt@gmail.com>
2023-07-20 17:38:17 +0630

    feat: setup chakra ui and path aliases

---
+++ packages/acquirer-frontend/src/theme/index.ts
@@ -0,0 +1,16 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  colors: {
+    primary: '#005585',
+    secondary: '#CCEDFF',
+    accent: '#FC440F',
+    info: '#3C8CFF',
+    danger: '#CE4911',
+    success: '#30D6A7',
+    warning: '#F9C937',
+    primaryBackground: '#E1E9F4',
+  },
+})
+
+export default theme
