d1a467af63c8c2234d59a6a6c926ed677617539d
Si Thu Myo <sithu.myo@thitsaworks.com>
2023-07-20 19:38:44 +0630

    [acquirer-backend] Add OpenAPI Documentation for health-check route

--- packages/acquirer-backend/src/routes/health-check-route.ts
+++ packages/acquirer-backend/src/routes/health-check-route.ts
@@ -1,5 +1,16 @@
 import express, { type Request, type Response } from 'express'
 
+/**
+ * @openapi
+ * /api/v1/health-check:
+ *   get:
+ *     tags:
+ *       - Health Check
+ *     summary: Health Check
+ *     responses:
+ *       200:
+ *         description: Health Check
+ */
 const router = express.Router()
 router.get('/health-check', (_req: Request, res: Response) => {
   res.send({ message: 'OK' })
