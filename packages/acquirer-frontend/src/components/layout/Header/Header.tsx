4f94dbc626d0645618449f45a6da13771e82a6b2
nainglinnkhant <nainglinnkhnt@gmail.com>
2023-07-22 00:13:21 +0630

    feat: add header content

--- packages/acquirer-frontend/src/components/layout/Header/Header.tsx
+++ packages/acquirer-frontend/src/components/layout/Header/Header.tsx
@@ -1,18 +1,57 @@
-import { HStack } from '@chakra-ui/react'
+import { HStack, IconButton, Image } from '@chakra-ui/react'
+import { FiMenu } from 'react-icons/fi'
+import { MdOutlineNotifications } from 'react-icons/md'
+
+import mojaloopLogo from '@/assets/mojaloop-logo.png'
+import { useDrawerDisclosure } from '@/context/DrawerDisclosureContext'
+import { Drawer } from '@/components/layout'
 
 const Header = () => {
+  const { onOpen } = useDrawerDisclosure()
+
   return (
     <HStack
       as='header'
-      w='full'
-      h='12'
-      justify={{ base: 'space-between', md: 'flex-end' }}
-      px='4'
+      w={{ base: 'full', md: 'calc(100% - 6rem)' }}
+      h='14'
+      justify='space-between'
+      ml={{ base: '0', md: '24' }}
+      px={{ base: '2', sm: '4', md: '6', lg: '8' }}
       shadow='sm'
       position='fixed'
       bg='white'
       zIndex='sticky'
-    ></HStack>
+    >
+      <IconButton
+        aria-label='Open sidebar'
+        icon={<FiMenu />}
+        display={{ base: 'flex', md: 'none' }}
+        fontSize='22px'
+        color='primary'
+        bg='transparent'
+        _hover={{ bg: 'secondary' }}
+        onClick={onOpen}
+      />
+
+      <Image
+        src={mojaloopLogo}
+        h='9'
+        display={{ base: 'none', md: 'block' }}
+        alt='Mojaloop Logo'
+      />
+
+      <Drawer />
+
+      <IconButton
+        aria-label='View notifications'
+        icon={<MdOutlineNotifications />}
+        variant='unstyled'
+        minW='auto'
+        mr='2'
+        color='accent'
+        fontSize='22px'
+      />
+    </HStack>
   )
 }
 
