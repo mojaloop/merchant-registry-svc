a675daaeb71699b2bb09478907e618e7f718f74a
nainglinnkhant <nainglinnkhnt@gmail.com>
2023-07-27 18:24:50 +0630

    refactor: separate input and select into form components

---
+++ packages/acquirer-frontend/src/components/form/FormInput/FormInput.tsx
@@ -0,0 +1,35 @@
+import {
+  FormControl,
+  type FormControlProps,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from '@chakra-ui/react'
+import type { FieldErrors, FieldValues, Path, UseFormRegister } from 'react-hook-form'
+
+interface FormInputProps<T extends FieldValues> extends FormControlProps {
+  name: Path<T>
+  register: UseFormRegister<T>
+  errors: FieldErrors<T>
+  label: string
+  placeholder: string
+}
+
+const FormInput = <T extends FieldValues>({
+  name,
+  register,
+  errors,
+  label,
+  placeholder,
+  ...props
+}: FormInputProps<T>) => {
+  return (
+    <FormControl isInvalid={!!errors[name]} maxW={{ md: '20rem' }} {...props}>
+      <FormLabel fontSize='sm'>{label}</FormLabel>
+      <Input {...register(name)} placeholder={placeholder} />
+      <FormErrorMessage>{errors[name]?.message?.toString() || ''}</FormErrorMessage>
+    </FormControl>
+  )
+}
+
+export default FormInput
---
+++ packages/acquirer-frontend/src/components/form/index.ts
@@ -0,0 +1,4 @@
+import FormInput from './FormInput/FormInput'
+import FormSelect from './FormSelect/FormSelect'
+
+export { FormInput, FormSelect }
