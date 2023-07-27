522a2662ec1069818d9e5df601a5225a6295556e
nainglinnkhant <nainglinnkhnt@gmail.com>
2023-07-28 04:24:02 +0630

    feat: add registry's location information form

--- packages/acquirer-frontend/src/lib/validations/registry.ts
+++ packages/acquirer-frontend/src/lib/validations/registry.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod'
+import { Countries } from 'shared-lib'
 
 export type BusinessInfo = z.infer<typeof businessInfoSchema>
+export type LocationInfo = z.infer<typeof locationInfoSchema>
 
 export const businessInfoSchema = z.object({
   businessName: z.string().nonempty({ message: 'Business name is required' }),
@@ -33,4 +35,26 @@ export const businessInfoSchema = z.object({
   ]),
   licenseNumber: z.string().optional(),
   licenseDocument: z.union([z.custom<File>(val => val instanceof File), z.null()]),
+})
+
+export const locationInfoSchema = z.object({
+  locationType: z.union([z.literal('physical'), z.literal('virtual')]),
+  country: z.nativeEnum(Countries).or(z.null()),
+  websiteUrl: z.string().optional(),
+  department: z.string().optional(),
+  subDepartment: z.string().optional(),
+  streetName: z.string().optional(),
+  buildingNumber: z.string().optional(),
+  buildingName: z.string().optional(),
+  floorNumber: z.string().optional(),
+  roomNumber: z.string().optional(),
+  postBox: z.string().optional(),
+  postalCode: z.string().optional(),
+  township: z.string().optional(),
+  district: z.string().optional(),
+  countrySubdivision: z.string().optional(),
+  physicalAddressCountry: z.nativeEnum(Countries),
+  longitude: z.string().optional(),
+  latitude: z.string().optional(),
+  checkoutDescription: z.string().optional(),
 })
--- packages/acquirer-frontend/src/pages/RegistryForm/LocationInfoForm.tsx
+++ packages/acquirer-frontend/src/pages/RegistryForm/LocationInfoForm.tsx
@@ -1,5 +1,278 @@
-const LocationInfoForm = () => {
-  return <div>LocationInfoForm</div>
+import { useEffect } from 'react'
+import { Box, Heading, SimpleGrid, Stack } from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { type LocationInfo, locationInfoSchema } from '@/lib/validations/registry'
+import { CustomButton } from '@/components/ui'
+import { FormInput, FormSelect } from '@/components/form'
+
+const LOCATION_TYPES = [
+  { value: 'physical', label: 'Physical' },
+  { value: 'virtual', label: 'Virtual' },
+]
+
+const COUNTRIES = [
+  { value: 'Afghanistan', label: 'Afghanistan' },
+  { value: 'Albania', label: 'Albania' },
+]
+
+interface LocationInfoFormProps {
+  setActiveStep: React.Dispatch<React.SetStateAction<number>>
+}
+
+const LocationInfoForm = ({ setActiveStep }: LocationInfoFormProps) => {
+  const {
+    register,
+    formState: { errors },
+    setFocus,
+    handleSubmit,
+  } = useForm<LocationInfo>({
+    resolver: zodResolver(locationInfoSchema),
+    defaultValues: {
+      country: null,
+    },
+  })
+
+  const onSubmit = (values: LocationInfo) => {
+    console.log(values)
+    setActiveStep(activeStep => activeStep + 1)
+  }
+
+  // focus on first input that has error after validation
+  useEffect(() => {
+    const firstError = Object.keys(errors)[0] as keyof LocationInfo
+
+    if (firstError) {
+      setFocus(firstError)
+    }
+  }, [errors, setFocus])
+
+  return (
+    <Stack as='form' onSubmit={handleSubmit(onSubmit)} pt='20' noValidate>
+      <SimpleGrid
+        templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
+        rowGap={{ base: '4', sm: '6' }}
+        pb='6'
+      >
+        <FormSelect
+          isRequired
+          name='locationType'
+          register={register}
+          errors={errors}
+          label='Location Type'
+          placeholder='Location Type'
+          options={LOCATION_TYPES}
+          errorMsg='Please select a location type'
+          justifySelf='center'
+        />
+
+        <FormSelect
+          name='country'
+          register={register}
+          errors={errors}
+          label='Country'
+          placeholder='Choose Country'
+          options={COUNTRIES}
+          errorMsg='Please select a country'
+          justifySelf='center'
+        />
+
+        <FormInput
+          name='websiteUrl'
+          register={register}
+          errors={errors}
+          label='Website URL'
+          placeholder='Website URL'
+          justifySelf='center'
+        />
+      </SimpleGrid>
+
+      <SimpleGrid
+        templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
+        rowGap={{ base: '4', sm: '6' }}
+        pt='2'
+        pb='6'
+      >
+        <Heading size='sm' as='h3' w='20rem' justifySelf='center'>
+          Physical Business Address
+        </Heading>
+      </SimpleGrid>
+
+      <SimpleGrid
+        templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
+        rowGap={{ base: '4', sm: '6' }}
+        pb='6'
+      >
+        <FormInput
+          name='department'
+          register={register}
+          errors={errors}
+          label='Department'
+          placeholder='Department'
+          justifySelf='center'
+        />
+
+        <FormInput
+          name='subDepartment'
+          register={register}
+          errors={errors}
+          label='Sub Department'
+          placeholder='Sub Department'
+          justifySelf='center'
+        />
+
+        <FormInput
+          name='streetName'
+          register={register}
+          errors={errors}
+          label='Street Name'
+          placeholder='Street Name'
+          justifySelf='center'
+        />
+
+        <FormInput
+          name='buildingNumber'
+          register={register}
+          errors={errors}
+          label='Building Number'
+          placeholder='Building Number'
+          justifySelf='center'
+        />
+
+        <FormInput
+          name='buildingName'
+          register={register}
+          errors={errors}
+          label='Building Name'
+          placeholder='Building Name'
+          justifySelf='center'
+        />
+
+        <FormInput
+          name='floorNumber'
+          register={register}
+          errors={errors}
+          label='Floor Number'
+          placeholder='Floor Number'
+          justifySelf='center'
+        />
+
+        <FormInput
+          name='roomNumber'
+          register={register}
+          errors={errors}
+          label='Room Number'
+          placeholder='Room Number'
+          justifySelf='center'
+        />
+
+        <FormInput
+          name='postBox'
+          register={register}
+          errors={errors}
+          label='Post Box'
+          placeholder='Post Box'
+          justifySelf='center'
+        />
+
+        <FormInput
+          name='postalCode'
+          register={register}
+          errors={errors}
+          label='Postal Code'
+          placeholder='Postal Code'
+          justifySelf='center'
+        />
+
+        <FormInput
+          name='township'
+          register={register}
+          errors={errors}
+          label='Township'
+          placeholder='Township'
+          justifySelf='center'
+        />
+
+        <FormInput
+          name='district'
+          register={register}
+          errors={errors}
+          label='District'
+          placeholder='District'
+          justifySelf='center'
+        />
+
+        <FormInput
+          name='countrySubdivision'
+          register={register}
+          errors={errors}
+          label='Country Subdivision (State/Divison)'
+          placeholder='Country Subdivision'
+          justifySelf='center'
+        />
+
+        <FormSelect
+          isRequired
+          name='physicalAddressCountry'
+          register={register}
+          errors={errors}
+          label='Country'
+          placeholder='Choose Country'
+          options={COUNTRIES}
+          errorMsg='Please select a country'
+          justifySelf='center'
+        />
+
+        <FormInput
+          name='longitude'
+          register={register}
+          errors={errors}
+          label='Longitude'
+          placeholder='Longitude'
+          justifySelf='center'
+        />
+
+        <FormInput
+          name='latitude'
+          register={register}
+          errors={errors}
+          label='Latitude'
+          placeholder='Latitude'
+          justifySelf='center'
+        />
+      </SimpleGrid>
+
+      <SimpleGrid
+        templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
+        rowGap={{ base: '4', sm: '6' }}
+        pb='12'
+      >
+        <FormInput
+          name='checkoutDescription'
+          register={register}
+          errors={errors}
+          label='Checkout Counter Description'
+          placeholder='Checkout Counter Description'
+          justifySelf='center'
+        />
+      </SimpleGrid>
+
+      <Box alignSelf='end'>
+        <CustomButton
+          ml='auto'
+          colorVariant='accent-outline'
+          w='32'
+          mr='4'
+          onClick={() => setActiveStep(activeStep => activeStep - 1)}
+        >
+          Back
+        </CustomButton>
+
+        <CustomButton type='submit'>Save and proceed</CustomButton>
+      </Box>
+    </Stack>
+  )
 }
 
 export default LocationInfoForm
