b0d2a9d6aa5aa3005b0c6400d4659a75ff1c0bee
Si Thu Myo <sithu.myo@thitsaworks.com>
2023-08-09 15:41:13 +0630

    fix: cannot access frontend localhost:5172 from docker-compose

--- packages/acquirer-frontend/vite.config.ts
+++ packages/acquirer-frontend/vite.config.ts
@@ -4,5 +4,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: process.env.VITE_HOST || '0.0.0.0',
+    port: parseInt(process.env.VITE_PORT || '5173'),
+  },
   plugins: [react(), tsconfigPaths()],
 })
