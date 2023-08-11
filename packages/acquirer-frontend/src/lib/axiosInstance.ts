4530c6b754b7458a0de8aa01ac454f5190937477
nainglinnkhant <nainglinnkhnt@gmail.com>
2023-08-11 15:18:39 +0630

    feat: add interceptor to axios instance

--- packages/acquirer-frontend/src/lib/axiosInstance.ts
+++ packages/acquirer-frontend/src/lib/axiosInstance.ts
@@ -1,7 +1,20 @@
-import axios from 'axios'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import axios, { type InternalAxiosRequestConfig } from 'axios'
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+})
+
+instance.interceptors.request.use(config => {
+  const token = sessionStorage.getItem('token')
+
+  if (config.url === '/users/login') return config
+
+  return {
+    ...config,
+    headers: { Authorization: `Bearer ${token}` },
+  } as InternalAxiosRequestConfig<any>
 })
 
 export default instance
