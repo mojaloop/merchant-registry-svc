09d68df5ea3c8ff0e9dd297d30217ef5a711b9a8
nainglinnkhant <nainglinnkhnt@gmail.com>
2023-08-15 18:17:19 +0630

    refactor: remove unnecessary utils function

--- packages/acquirer-frontend/src/utils/index.ts
+++ packages/acquirer-frontend/src/utils/index.ts
@@ -1,14 +1,5 @@
 import type { MerchantInfo } from '@/types/merchants'
 import type { MerchantDetails } from '@/types/merchantDetails'
-import capitalize from 'lodash.capitalize'
-
-export const convertKebabCaseToReadable = (snakeCaseString: string) => {
-  return snakeCaseString
-    .replace(/-/g, ' ')
-    .split(' ')
-    .map(word => capitalize(word))
-    .join(' ')
-}
 
 export const scrollToTop = () => {
   document.getElementById('main')?.scrollTo({ top: 0, behavior: 'smooth' })
