03e786a1292589d14b1fa57f3e5d0751b91ef13d
nainglinnkhant <nainglinnkhnt@gmail.com>
2023-08-24 17:37:51 +0630

    chore: setup tanstack query

--- packages/acquirer-frontend/src/main.tsx
+++ packages/acquirer-frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '@/theme'
@@ -8,14 +10,25 @@ import DrawerDisclosureProvider from '@/context/DrawerDisclosureContext.tsx'
 import App from './App.tsx'
 import './index.css'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60,
+    },
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <DrawerDisclosureProvider>
-          <App />
-        </DrawerDisclosureProvider>
-      </ChakraProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <BrowserRouter>
+        <ChakraProvider theme={theme}>
+          <DrawerDisclosureProvider>
+            <App />
+          </DrawerDisclosureProvider>
+        </ChakraProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 )
