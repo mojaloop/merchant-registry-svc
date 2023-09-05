fc7b966913db1280fe4877eb3d98888ac3919302
tw-sithumyo <sithu.myo@thitsaworks.com>
2023-09-05 15:03:55 +0630

    [acquirer-backend] GET Users include Role and Permissions

--- packages/acquirer-backend/src/routes/user-controllers/get-users.ts
+++ packages/acquirer-backend/src/routes/user-controllers/get-users.ts
@@ -24,6 +24,21 @@ export async function getUsers (req: AuthRequest, res: Response) {
     return res.status(401).send({ message: 'Unauthorized' })
   }
 
-  const users = await AppDataSource.manager.find(PortalUserEntity)
-  res.send({ message: 'List of users', data: users })
+  const users = await AppDataSource.manager.find(PortalUserEntity, {
+    relations: ['role', 'role.permissions']
+  })
+
+  const flattenedUsers = users.map(user => {
+    return {
+      ...user,
+      role: {
+        ...user.role,
+        created_at: undefined,
+        updated_at: undefined,
+        permissions: user.role.permissions.map(permission => permission.name)
+      },
+      password: undefined
+    }
+  })
+  res.send({ message: 'List of users', data: flattenedUsers })
 }
