33e75228cffb15ab87670d7c5e78f0f2dd338d31
nainglinnkhant <nainglinnkhnt@gmail.com>
2023-10-10 16:56:39 +0630

    feat: add profile modal in header

--- packages/acquirer-frontend/src/components/layout/Header/Header.tsx
+++ packages/acquirer-frontend/src/components/layout/Header/Header.tsx
@@ -1,10 +1,27 @@
 import { useNavigate } from 'react-router-dom'
 import { useQueryClient } from '@tanstack/react-query'
-import { HStack, IconButton, Image, Tooltip } from '@chakra-ui/react'
+import {
+  Avatar,
+  Box,
+  Button,
+  HStack,
+  Icon,
+  IconButton,
+  Image,
+  Popover,
+  PopoverBody,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  Text,
+} from '@chakra-ui/react'
 import { FiMenu } from 'react-icons/fi'
 import { TbLogout } from 'react-icons/tb'
+import { AiOutlineMail, AiOutlineUser } from 'react-icons/ai'
+import { MdLockOutline } from 'react-icons/md'
 
 import mojaloopLogo from '@/assets/mojaloop-logo.png'
+import { useUserProfile } from '@/api/hooks/users'
 import { useDrawerDisclosure } from '@/contexts/DrawerDisclosureContext'
 import { Drawer } from '@/components/layout'
 
@@ -12,6 +29,8 @@ const Header = () => {
   const navigate = useNavigate()
   const queryClient = useQueryClient()
   const { onOpen } = useDrawerDisclosure()
+
+  const userProfile = useUserProfile()
 
   return (
     <HStack
@@ -53,7 +72,81 @@ const Header = () => {
           fontSize='22px'
         /> */}
 
-        <Tooltip label='Logout' hasArrow bg='primary'>
+        <Popover offset={[-125, 0]}>
+          <PopoverTrigger>
+            <Button variant='unstyled'>
+              <Avatar name={userProfile.data?.name || ''} size='sm' />
+            </Button>
+          </PopoverTrigger>
+
+          <PopoverContent shadow='lg' w='72'>
+            <PopoverHeader fontWeight='medium' borderColor='gray.100'>
+              Profile
+            </PopoverHeader>
+
+            <PopoverBody display='flex' flexDir='column'>
+              <HStack spacing='2' p='1.5'>
+                <Box w='6' h='6' display='flex' justifyItems='center' alignItems='center'>
+                  <Icon as={AiOutlineUser} w='full' />
+                </Box>
+                <Text fontSize='sm'>{userProfile.data?.name || ''}</Text>
+              </HStack>
+
+              <HStack spacing='2' p='1.5'>
+                <Box w='6' h='6' display='flex' justifyItems='center' alignItems='center'>
+                  <Icon as={AiOutlineMail} w='full' />
+                </Box>
+                <Text fontSize='sm'>{userProfile.data?.email || ''}</Text>
+              </HStack>
+
+              <Button variant='unstyled' h='auto' p='1.5' _hover={{ bg: 'gray.100' }}>
+                <HStack spacing='2'>
+                  <Box
+                    w='6'
+                    h='6'
+                    display='flex'
+                    justifyItems='center'
+                    alignItems='center'
+                  >
+                    <Icon as={MdLockOutline} w='full' />
+                  </Box>
+                  <Box as='span' fontSize='sm' fontWeight='normal'>
+                    Change Password
+                  </Box>
+                </HStack>
+              </Button>
+
+              <Button
+                variant='unstyled'
+                h='auto'
+                p='1.5'
+                _hover={{ bg: 'gray.100' }}
+                onClick={() => {
+                  localStorage.removeItem('token')
+                  navigate('/login')
+                  queryClient.removeQueries()
+                }}
+              >
+                <HStack spacing='2'>
+                  <Box
+                    w='6'
+                    h='6'
+                    display='flex'
+                    justifyItems='center'
+                    alignItems='center'
+                  >
+                    <Icon as={TbLogout} w='full' />
+                  </Box>
+                  <Box as='span' fontSize='sm' fontWeight='normal'>
+                    Logout
+                  </Box>
+                </HStack>
+              </Button>
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
+
+        {/* <Tooltip label='Logout' hasArrow bg='primary'>
           <IconButton
             aria-label='Logout'
             icon={<TbLogout />}
@@ -69,7 +162,7 @@ const Header = () => {
               queryClient.removeQueries()
             }}
           />
-        </Tooltip>
+        </Tooltip> */}
       </HStack>
     </HStack>
   )
