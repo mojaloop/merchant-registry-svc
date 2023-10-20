21f14f702d96201f43a4949b9aecad5fc314d09c
nainglinnkhant <nainglinnkhnt@gmail.com>
2023-10-20 20:45:14 +0630

    chore: setup vitest

--- packages/acquirer-frontend/vite.config.ts
+++ packages/acquirer-frontend/vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
@@ -9,4 +12,9 @@ export default defineConfig({
     port: parseInt(process.env.VITE_PORT || '5173'),
   },
   plugins: [react(), tsconfigPaths()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/__tests__/setupTests.ts'],
+  },
 })
