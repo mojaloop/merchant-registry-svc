6bdb6a10d6d3616608658f98b65c23efcdc92494
nainglinnkhant <nainglinnkhnt@gmail.com>
2023-10-31 22:02:17 +0630

    feat: add chakra provider to test wrapper

--- packages/acquirer-frontend/src/__tests__/TestWrapper.tsx
+++ packages/acquirer-frontend/src/__tests__/TestWrapper.tsx
@@ -1,15 +1,24 @@
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import { DrawerDisclosureProvider, NavItemsProvider } from '@/contexts'
+import theme from '@/theme'
 
 const TestWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={new QueryClient()}>
       <BrowserRouter>
-        <NavItemsProvider>
-          <DrawerDisclosureProvider>{children}</DrawerDisclosureProvider>
-        </NavItemsProvider>
+        <ChakraProvider
+          theme={theme}
+          toastOptions={{
+            defaultOptions: { variant: 'subtle', position: 'top', isClosable: true },
+          }}
+        >
+          <NavItemsProvider>
+            <DrawerDisclosureProvider>{children}</DrawerDisclosureProvider>
+          </NavItemsProvider>
+        </ChakraProvider>
       </BrowserRouter>
     </QueryClientProvider>
   )
