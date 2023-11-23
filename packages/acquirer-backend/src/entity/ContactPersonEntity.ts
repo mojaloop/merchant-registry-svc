baa1051d418946ec3efdd5345fe37ead7ecf1511
tw-sithumyo <133853146+tw-sithumyo@users.noreply.github.com>
2023-11-23 13:59:13 +0630

    refactor(orm): remove the redundent businessPersonLocation field that already exists in PersonEntity (#173)

--- packages/acquirer-backend/src/entity/ContactPersonEntity.ts
+++ packages/acquirer-backend/src/entity/ContactPersonEntity.ts
@@ -1,15 +1,11 @@
 import {
-  Entity, ManyToOne, OneToOne
+  Entity, ManyToOne
 } from 'typeorm'
-import { BusinessPersonLocationEntity } from './BusinessPersonLocationEntity'
 import { MerchantEntity } from './MerchantEntity'
 import { PersonEntity } from './PersonEntity'
 
 @Entity('contact_persons')
 export class ContactPersonEntity extends PersonEntity {
-  @OneToOne(() => BusinessPersonLocationEntity)
-    businessPersonLocation!: BusinessPersonLocationEntity
-
   @ManyToOne(
     () => MerchantEntity,
     merchant => merchant.contact_persons,
