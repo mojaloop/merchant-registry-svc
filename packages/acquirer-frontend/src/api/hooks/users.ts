6ad9b4be830a111bdc0e27fc5170e7ab71e5a90e
tw-sithumyo <133853146+tw-sithumyo@users.noreply.github.com>
2024-01-12 15:41:16 +0630

    feat: add activate/disable/block btns, dfsp column, status icons in User Mgmt UI (#228)
    * feat: add activate/disable/block btns, dfsp column, status icons in User Mgmt UI

--- packages/acquirer-frontend/src/api/hooks/users.ts
+++ packages/acquirer-frontend/src/api/hooks/users.ts
@@ -3,7 +3,7 @@ import { useToast } from '@chakra-ui/react'
 import { isAxiosError } from 'axios'
 
 import { FALLBACK_ERROR_MESSAGE } from '@/constants/errorMessage'
-import { createUser, getUserProfile, getUsers } from '../users'
+import { createUser, getUserProfile, getUsers, updateUserStatus } from '../users'
 
 export function useUsers() {
   return useQuery({
@@ -43,6 +43,38 @@ export function useCreateUser() {
       })
     },
   })
+}
+
+export function useUserStatusUpdate() {
+  const queryClient = useQueryClient()
+  const toast = useToast()
+
+  const mutation = useMutation({
+    mutationFn: (data: { userId: string | number; newStatus: string }) =>
+      updateUserStatus(data.userId, data.newStatus),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['users'] })
+      toast({
+        title: 'User Status Update Successful!',
+        description: 'User status has been updated successfully.',
+        status: 'success',
+      })
+    },
+    onError: error => {
+      let errorMessage = FALLBACK_ERROR_MESSAGE
+      if (isAxiosError(error) && typeof error.response?.data.message === 'string') {
+        errorMessage = error.response.data.message
+      }
+      toast({
+        title: 'User Status Update Failed!',
+        description: errorMessage,
+        status: 'error',
+      })
+    },
+  })
+
+  return mutation
 }
 
 export function useUserProfile() {
